91cbbb6d4eec09d64f6ea34b32ac18337c91d848
Abdallahafez875 <121691680+Abdallahafez875@users.noreply.github.com>
2022-12-30 21:34:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the First Number");
+            int N1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int N2 = int.Parse(Console.ReadLine());
+            int N3 = 0;
+            int p = 0;
+            int mul = 1;
+            for (N3 = N1 + 1; N3 < N2; N3++)
+            {
+
+                for (p = 1; p <= N3 / 2; p++)
+                {
+                    mul *= N3 % (p + 1);
+                }
+                if (mul > 0)
+                {
+                    Console.WriteLine(N3);
+                }
+                mul = 1;
+            }
+
+        }
+    }
+}
